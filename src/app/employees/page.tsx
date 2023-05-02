e2a86368e778ebd87adc926fae9e4433aa4ddfe5
sneakyspeedyboii <98123917+sneakyspeedyboii@users.noreply.github.com>
2023-05-02 14:00:38 +0100

    Suitable for production (real)

--- src/app/employees/page.tsx
+++ src/app/employees/page.tsx
@@ -1,5 +1,7 @@
 import styles from './page.module.css';
 
+import Image from 'next/image';
+
 export default function Employees() {
     return (
         <main className={styles.main}>
@@ -8,6 +10,19 @@ export default function Employees() {
                     <div className={styles.title}>Our Employees</div>
                     <div className={styles.sub_header}>Here are some of our current workers</div>
                     <div className={styles.sub_sub_header}>We like to be clear and transparent at our company, so we allowed <br /> each employee to talk about themselves for a bit!</div>
+                </div>
+                <div className={styles.employees}>
+
+                    <div className={styles.employee}>
+                        <div className={styles.employee_info_container}>
+                            <div className={styles.name}>John Doe</div>
+                            <div className={styles.position}>CEO</div>
+                            <div className={styles.company_info}>Our thoughts: good ol john, we get paid to do the work for him, and then sometimes hes make decisions on what the company does.</div>
+                            <div className={styles.personal_description}>"Man i love hot beans" - John Doe</div>
+                            <div className={styles.email}>john@hotbeans.org.uk</div>
+                        </div>
+                        <div className={styles.image_container}><Image src={"/employee/man1.png"} width={"100"} height={"25"}></Image></div>
+                    </div>
                 </div>
             </div>
         </main>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,54 @@
+"use client"
+
 import './globals.css'
+import styles from './window.module.css'
+
+import { useState } from 'react';
 
 export const metadata = {
   title: 'Hot Beans',
   description: 'good ol company website',
 }
 
+
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
-  return (
-    <html lang="en">
+
+  const [renderWindow, setRenderWindow] = useState(true);
+
+  function handle() {
+    document.body.style.overflow = "auto";
+    setRenderWindow(false);
+
+  }
+
+  if (renderWindow) {
+    return (
+      <html lang="en">
+        <body>
+          {children}
+          <div className={styles.container}>
+            <div className={styles.window_container}>
+              <h1>Info about site</h1>
+              <div>Website made for assignment and very un-finished</div>
+              <div>Ran out of time to perfect the site so this a functional but not the prettiest website.</div>
+              <button onClick={() => handle()}>I understand</button>
+            </div>
+          </div>
+        </body>
+      </html>
+    )
+  } else {
+    return (
+      <html lang="en">
       <body>
         {children}
       </body>
     </html>
-  )
+    )
+  }
 }
