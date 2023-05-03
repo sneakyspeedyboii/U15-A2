730f4379a92df2a42de8aa04acc43d4195a097f9
sneakyspeedyboii <98123917+sneakyspeedyboii@users.noreply.github.com>
2023-05-03 19:59:42 +0100

    Main page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,10 @@
-"use client"
-
 import './globals.css'
-import styles from './window.module.css'
+import Layout from './components/layout/layout';
 
-import { useState } from 'react';
-import Script from 'next/script'
-
-
+export const metadata = {
+  title: 'Hot Beans',
+  description: 'good ol company website',
+}
 
 export default function RootLayout({
   children,
@@ -14,38 +12,14 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
 
-  const [renderWindow, setRenderWindow] = useState(true);
-
-  function handle() {
-    document.body.style.overflow = "auto";
-    setRenderWindow(false);
-
-  }
-
-  if (renderWindow) {
-    return (
-      <html lang="en">
-        <body>
-          {children}
-          <div className={styles.container}>
-            <div className={styles.window_container}>
-              <h1>Info about site</h1>
-              <div>Website made for assignment and very un-finished</div>
-              <div>Ran out of time to perfect the site so this a functional but not the prettiest website.</div>
-              <button onClick={() => handle()}>I understand</button>
-            </div>
-          </div>
-          <Script async src="https://aly.felixklg.dev/VwDrVUy9RFqbFd6" data-website-id="68070bc2-d1c7-4d76-a738-51b7fd540403"></Script>
-        </body>
-      </html>
-    )
-  } else {
-    return (
-      <html lang="en">
+  return (
+    <html lang="en">
       <body>
-        {children}
+        <Layout>
+          {children}
+        </Layout>
       </body>
     </html>
-    )
-  }
+  )
+
 }
