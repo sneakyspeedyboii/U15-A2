c20b0dba5cda907c1613abbe17c143cdb7061a65
sneakyspeedyboii <98123917+sneakyspeedyboii@users.noreply.github.com>
2023-05-04 21:40:09 +0100

    Minimum front page completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,80 @@
 "use client"
 
 import styles from './page.module.css'
-
-import Image from 'next/image'
-import Link from 'next/link'
-import { useKeenSlider } from "keen-slider/react"
-import "keen-slider/keen-slider.min.css"
-
 import React from '../../public/home/slider/react.png';
 import Vue from '../../public/home/slider/vue.png';
 import Angular from '../../public/home/slider/angular.png';
 import Svelte from '../../public/home/slider/svelte.png';
 import Solid from '../../public/home/slider/solid.png';
 
+import Stripe from '../../public/home/cards/stripe.png';
+import NextJS from '../../public/home/cards/nextjs.png';
+import Turbo from '../../public/home/cards/turbo.png';
+
+import Image from 'next/image'
+import Link from 'next/link'
+import { useKeenSlider } from "keen-slider/react"
+import "keen-slider/keen-slider.min.css"
+import { Variants, motion } from "framer-motion"
+
 const animation = { duration: 15000, easing: (t: number) => t }
+
+const card1: Variants = {
+  offscreen: {
+    y: "100%",
+    x: "50%",
+    opacity: "0"
+  },
+  onscreen: {
+    y: "50%",
+    x: "5%",
+    rotate: -20,
+    opacity: "100%",
+    transition: {
+      type: "spring",
+      bounce: 0.4,
+      duration: 1.5
+    }
+  }
+};
+
+const card2: Variants = {
+  offscreen: {
+    y: "100%",
+    x: "70%",
+    opacity: "0"
+  },
+  onscreen: {
+    y: "20%",
+    x: "70%",
+    rotate: 0,
+    opacity: "100%",
+    transition: {
+      type: "spring",
+      bounce: 0.4,
+      duration: 1.5
+    }
+  }
+};
+
+const card3: Variants = {
+  offscreen: {
+    y: "100%",
+    x: "100%",
+    opacity: "0"
+  },
+  onscreen: {
+    y: "40%",
+    x: "135%",
+    rotate: 40,
+    opacity: "100%",
+    transition: {
+      type: "spring",
+      bounce: 0.4,
+      duration: 1.5
+    }
+  }
+};
 
 export default function Home() {
   const [sliderRef] = useKeenSlider<HTMLDivElement>({
@@ -50,10 +111,6 @@ export default function Home() {
           <div className={styles.link_container}>
             <Link className={styles.link} href={"/"}>Meet the team</Link>
             <Link className={styles.link} href={"/"}>Opportunities here</Link>
-          </div>
-          <div>
-            <div>Explore</div>
-            <div>svg here</div>
           </div>
         </div>
       </div>
@@ -93,11 +150,41 @@ export default function Home() {
         </div>
 
         <div className={styles.info2}>
-          
+          <div className={styles.left}>
+            <motion.div
+              className={styles.effect_container}
+              initial="offscreen"
+              whileInView="onscreen"
+              viewport={{ once: false, amount: 0.8 }}
+            >
+              <motion.div className={styles.card} variants={card1}>
+                <div className={styles.next}>
+                  <Image src={NextJS} alt='NextJS' fill={true} />
+                </div>
+              </motion.div>
+              <motion.div className={styles.card} variants={card2}>
+                <div className={styles.stripe}>
+                  <Image src={Stripe} alt='Stripe' fill={true} />
+                </div>
+              </motion.div>
+              <motion.div className={styles.card} variants={card3}>
+                <div className={styles.turbo}>
+                  <Image src={Turbo} alt='Turbo' fill={true} />
+                </div>
+              </motion.div>
+
+            </motion.div>
+          </div>
+
+          <div className={styles.right}>
+            <div className={styles.header}>The #1 Trusted Company</div>
+            <div className={styles.sub_header}>Rest easy knowing the outcome will be perfect!</div>
+
+            <div className={styles.body}>Here, at Hot Beans Web Development, we have built, tested, and deployed websites for some of the biggest industries out there.</div>
+
+          </div>
         </div>
       </div>
-
-
 
     </main>
   )
